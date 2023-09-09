b47544806ce18254a448cb7aa5dd2c54fdbb830b
yellow sky <hellun515@gmail.com>
2023-09-09 18:24:18 +0900

    upload file api respose

--- hatuvue-backend/src/routes/file.ts
+++ hatuvue-backend/src/routes/file.ts
@@ -5,7 +5,15 @@ const router = express.Router();
 
 router.post("/upload", upload.single("file"), (req, res) => {
   const file = req.file;
-  res.send({ message: "Ok", data: req.file });
+  res.send({
+    message: "Ok",
+    data: {
+      name: req.file?.originalname,
+      type: req.file?.mimetype,
+      uuid: req.file?.filename,
+      size: req.file?.size,
+    },
+  });
 });
 
 module.exports = router;
