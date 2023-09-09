cb5956f4ef18d93212c9b70abf84e12cd31ef7b0
yellow sky <hellun515@gmail.com>
2023-09-09 16:43:19 +0900

    git ignore .env

--- hatuvue-backend/src/util/video.ts
+++ hatuvue-backend/src/util/video.ts
@@ -1,10 +1,38 @@
+import { UUID } from "crypto";
 import { query } from "../connection";
 
-const findVideo = async (id: number) => {
-  const result = await query("SELECT * FROM videos WHERE id=?", id);
-  return result[0];
+interface VideoData {
+  id: number;
+  name: string;
+  description: string;
+  uploadAt: Date;
+  author: string;
+  fileUuid: UUID;
+  like: number[];
+  disLike: number[];
+}
+
+const findVideo = async (id: number): Promise<VideoData> => {
+  const result = (await query("SELECT * FROM videos WHERE id=?", id))[0];
+  return {
+    id: result.id,
+    name: result.name,
+    description: result.description,
+    uploadAt: result.uploadat,
+    author: result.author,
+    fileUuid: result.filduuid,
+    like: JSON.parse(result.like),
+    disLike: JSON.parse(result.dislike),
+  };
 };
 
-const createVideo = async () => {};
+const createVideo = async (
+  name: string,
+  description: string,
+  author: string,
+  file: File
+) => {
+  query("");
+};
 
-export { findVideo };
+export { findVideo, VideoData, createVideo };
